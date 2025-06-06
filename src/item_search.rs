591e68ad35f9f46bcc6fc7d3188f65094c7780eb
dandels <dandels@hotmail.com>
2025-06-07 00:20:25 +0300

    Fix search broken by adding colors too early
    ANSI escape sequences broke the string matching so strings have to be
    matched before they can be added

--- src/item_search.rs
+++ src/item_search.rs
@@ -10,7 +10,7 @@ pub type LocalizationStrings = HashMap<String, String>;
 
 #[derive(Debug, Default)]
 pub struct TagNames {
-    pub items: HashMap<String, EntryType>,
+    pub items: HashMap<String, (EntryType, Vec<Option<u32>>)>,
     pub affixes: HashMap<String, EntryType>,
 }
 
@@ -21,14 +21,66 @@ pub struct ItemLookup {
 }
 
 pub struct CompleteItem {
-    name: ColoredString,
-    prefix: Option<ColoredString>,
-    suffix: Option<ColoredString>,
+    name: String,
+    item_rarity: Rarity,
+    prefix: Option<String>,
+    prefix_rarity: Rarity,
+    suffix: Option<String>,
+    suffix_rarity: Rarity,
     level_req: Option<u32>,
     quantity: u32,
 }
 
-impl CompleteItem {}
+enum Rarity {
+    Legendary,
+    Epic,
+    Rare,
+    RareComponent,
+    Magical,
+    CommonOrUnknown,
+}
+
+impl From<&String> for Rarity {
+    fn from(string: &String) -> Self {
+        match string.to_lowercase().as_str() {
+            "legendary" => Self::Legendary,
+            "rare" => Self::Rare,
+            "epic" => Self::Epic,
+            "magical" => Self::Magical,
+            _ => Self::CommonOrUnknown,
+        }
+    }
+}
+
+fn color_item_by_rarity(string: String, rarity: &Rarity) -> ColoredString {
+    match rarity {
+        Rarity::Legendary => string.purple(),
+        Rarity::Epic => string.bright_blue(),
+        Rarity::Rare => string.bright_green(),
+        Rarity::RareComponent => string.yellow(),
+        Rarity::Magical => string.bright_yellow(),
+        Rarity::CommonOrUnknown => ColoredString::from(string),
+    }
+}
+
+fn color_affix_by_rarity(string: String, rarity: &Rarity) -> ColoredString {
+    match rarity {
+        Rarity::Rare => string.green(),
+        Rarity::Magical => string.yellow(),
+        _ => ColoredString::from(string),
+    }
+}
+
+impl CompleteItem {
+    fn fmt_searchable_item_name(&self) -> String {
+        format!(
+            "{} {} {}", // correct amount of whitespace is not important for search
+            self.prefix.as_ref().unwrap_or(&"".into()),
+            &self.name,
+            self.suffix.as_ref().unwrap_or(&"".into())
+        )
+    }
+}
 
 impl Display for CompleteItem {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -47,57 +99,39 @@ impl Display for CompleteItem {
             }
         };
 
-        let name = &self.name;
+        let name_colored = color_item_by_rarity(self.name.clone(), &self.item_rarity);
+        let suffix_colored = color_affix_by_rarity(self.suffix.clone().unwrap_or("".to_string()), &self.suffix_rarity);
+        let prefix_colored = color_affix_by_rarity(self.prefix.clone().unwrap_or("".to_string()), &self.prefix_rarity);
         if self.prefix.is_none() {
-            write!(
-                f,
-                "{lvl_req} {quantity}{name} {}",
-                self.suffix.as_ref().unwrap_or(&"".into())
-            )
+            write!(f, "{lvl_req} {quantity}{name_colored} {suffix_colored}")
         } else {
-            write!(
-                f,
-                "{lvl_req} {} {name} {}",
-                self.prefix.as_ref().unwrap(),
-                self.suffix.as_ref().unwrap_or(&"".into())
-            )
+            write!(f, "{lvl_req} {prefix_colored} {name_colored} {suffix_colored}")
         }
     }
 }
 
 impl ItemLookup {
     pub fn lookup_item(&self, inventory_item: &InventoryItem) -> Option<CompleteItem> {
-        if let Some(EntryType::Item(_record_name, tag_name, rarity, level_req)) =
+        if let Some((EntryType::Item(_record_name, tag_name, item_rarity, level_req), ilvls)) =
             self.tag_names.items.get(&inventory_item.base_name)
         {
             if let Some(item_name) = self.localization_data.get(tag_name) {
-                // Clone instead of get_mut() since localization_data is shared between threads
-                let mut item_name = item_name.clone();
-                let colored_name = match rarity.as_str() {
-                    "Legendary" => item_name.purple(),
-                    "Rare" => {
-                        // Rare components have this for some reason...
-                        if item_name.starts_with("^k") {
-                            item_name.drain(0..2);
-                            item_name.yellow()
-                        } else {
-                            item_name.bright_green()
-                        }
-                    }
-                    "Epic" => item_name.bright_blue(),
-                    "Magical" => item_name.bright_yellow(),
-                    // ... is there really no API to construct a ColoredString without a style..?
-                    _ => {
-                        let mut cs = item_name.red();
-                        cs.fgcolor = None;
-                        cs
-                    }
-                };
+                // Uncomment to get record name and tag name of an item that the player has
+                //if item_name == "Baldir's Mantle" {
+                //    //println!("mantle is {record_name}, {tag_name}");
+                //    println!("{:?}", inventory_item);
+                //}
+                // TODO fix this logic, hashmap needs to count tagnames and not record names
+                if ilvls.len() > 1 {
+                    println!("{item_name} has {} tiers", ilvls.len());
+                }
+
                 let mut prefix: Option<String> = None;
-                let mut colored_prefix: Option<ColoredString> = None;
+                let mut prefix_rarity = Rarity::CommonOrUnknown;
                 if !inventory_item.prefix_name.is_empty() {
                     let tag_prefix = self.tag_names.affixes.get(&inventory_item.prefix_name);
                     if let Some(EntryType::Affix(affix_info)) = tag_prefix {
+                        prefix_rarity = Rarity::from(&affix_info.rarity);
                         if let Some(affix_name) = &affix_info.name {
                             prefix = Some(affix_name.clone());
                         } else if let Some(tag_name) = &affix_info.tag_name {
@@ -105,20 +139,14 @@ impl ItemLookup {
                                 prefix = Some(name.clone());
                             }
                         }
-                        if let Some(p) = prefix {
-                            if affix_info.rarity.to_lowercase() == "rare" {
-                                colored_prefix = Some(p.green());
-                            } else if affix_info.rarity.to_lowercase() == "magical" {
-                                colored_prefix = Some(p.yellow());
-                            }
-                        }
                     }
                 }
                 let mut suffix = None;
-                let mut colored_suffix = None;
+                let mut suffix_rarity = Rarity::CommonOrUnknown;
                 if !inventory_item.suffix_name.is_empty() {
                     let tag_suffix = self.tag_names.affixes.get(&inventory_item.suffix_name);
                     if let Some(EntryType::Affix(affix_info)) = tag_suffix {
+                        suffix_rarity = Rarity::from(&affix_info.rarity);
                         if let Some(name) = &affix_info.name {
                             suffix = Some(name.clone());
                         } else if let Some(tag_name) = &affix_info.tag_name {
@@ -126,22 +154,29 @@ impl ItemLookup {
                                 suffix = Some(name.clone());
                             }
                         }
-                        if let Some(s) = suffix {
-                            if affix_info.rarity.to_lowercase() == "rare" {
-                                colored_suffix = Some(s.green());
-                            } else if affix_info.rarity.to_lowercase() == "magical" {
-                                colored_suffix = Some(s.yellow());
-                            } else {
-                                println!("{}", affix_info.rarity);
-                            }
-                        }
                     }
                 }
                 let quantity = inventory_item.stack_count;
+
+                let mut item_name = item_name.clone();
+                let item_rarity = {
+                    /* Rare components have this for some reason... Let's give them their own color since we aren't
+                     * detecting them in any other way. */
+                    if item_name.starts_with("^k") {
+                        item_name.drain(0..2);
+                        Rarity::RareComponent
+                    } else {
+                        Rarity::from(item_rarity)
+                    }
+                };
+
                 Some(CompleteItem {
-                    name: colored_name,
-                    prefix: colored_prefix,
-                    suffix: colored_suffix,
+                    name: item_name,
+                    item_rarity,
+                    prefix,
+                    prefix_rarity,
+                    suffix,
+                    suffix_rarity,
                     level_req: *level_req,
                     quantity,
                 })
@@ -155,7 +190,7 @@ impl ItemLookup {
 
     pub fn check_item(&self, inventory_item: &InventoryItem, item_source: &str) {
         if let Some(ci) = self.lookup_item(inventory_item) {
-            let item_name = ci.to_string();
+            let item_name = ci.fmt_searchable_item_name();
             if item_name.to_lowercase().contains(&self.search_term) {
                 // Most of print logic is handled inside CompleteItem
                 println!("{item_source}: {ci}");
